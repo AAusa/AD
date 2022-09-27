e16fbf83eedfa1eebdd6314eb91800c2fbc037ca
alu <alu@PC64>
2022-09-27 10:01:26 +0200

    Ej3, avances ej4

--- FicherosAlvaro/src/gestionFichero/Ej4/Ej4.java
+++ FicherosAlvaro/src/gestionFichero/Ej4/Ej4.java
@@ -1,9 +1,11 @@
 package gestionFichero.Ej4;
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 public class Ej4 {
 	public static void main(String[] args) {
 		List<String> frases = new ArrayList<String>();
@@ -13,11 +15,26 @@ public class Ej4 {
 		while(!terminar) {
 			System.out.print("Frase: ");
 			frase = teclado.nextLine();
-			if(frase != "fin") {
-				frases.add(frase);
+			if(frase.equals("fin")) {
+				File mifichero = new File("ficheroEj4.txt");
+
+				try {
+					FileWriter escritor=new FileWriter(mifichero); // Sin true no concatena
+					BufferedWriter cestaEscritura=new BufferedWriter(escritor);
+					for(String f : frases) {
+						cestaEscritura.write(frase);
+					}
+					cestaEscritura.close();  
+					escritor.close();
+				}
+				catch (IOException ex) {
+					ex.printStackTrace();
+					System.out.println("No se encontro el archivo");
+				}
+				terminar = true;
 			}
 			else {
-				terminar = true;
+				frases.add(frase);
 			}
 		}
 	}
