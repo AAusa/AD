a92243b607cf7a9f41e2032f61e53a34e60bd6a4
AAusa <aa21.aausaberri87@iespabloserrano.com>
2023-02-03 10:17:59 +0100

    Ej1ExistDBterminado

--- ExistDBEj1/src/existDB/Ej1.java
+++ ExistDBEj1/src/existDB/Ej1.java
@@ -11,15 +11,27 @@ import org.xmldb.api.base.ResourceIterator;
 import org.xmldb.api.base.ResourceSet;
 import org.xmldb.api.base.XMLDBException;
 import org.xmldb.api.modules.XPathQueryService;
+/**
+ * Implementa un programa en Java que acceda a la BD que has creado para solucionar el ejercicio 1 y muestre un menú donde resuelva las siguientes consultas:
 
+Obtener los apellidos distintos de los autores.
+Obtener los nodos libro que tengan un nodo editor.
+Obtener los títulos de los libros cuyo precio esté por debajo de 50 euros.
+Por cada libro con autores, seleccionar el título del libro y sus autores. Si el libro no tiene autores, pero el editor sí, la consulta devolverá el título del libro y la afiliación del editor.
+ELIMINA LAS ETIQUETAS PARA QUE EN EL RESULTADO EN LA APLICACIÓN JAVA NO MUESTRE COMO SALIDA UN FICHERO XML (puedes hacerlo diréctamente en la consulta o usando una función que lea un fichero xml y devuelva los textos)
+ * @author Álvaro
+ *
+ */
 
 public class Ej1 {
 
+	/**
+	 * Menu:
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// driver que necesito para conectar existDB
 		String driver  =  "org.exist.xmldb.DatabaseImpl";
 		Collection miDB = null;
-		// cargar el driver
 		Class cl;
 		Scanner sn = new Scanner(System.in);
 		boolean salir = false;
@@ -27,14 +39,10 @@ public class Ej1 {
 		Ej1 ej1 = new Ej1();
 		try {
 			cl = Class.forName(driver);
-			
-			//instancia de la BD
 			Database BD = (Database)cl.newInstance();
 			
-			// registrar el Driver
 	        DatabaseManager.registerDatabase(BD);
 	        
-	        // La cadena de conexion
 	        String URI = "xmldb:exist://192.168.56.104:8080/exist/xmlrpc/db/pruebas";
 	        
 	        miDB = DatabaseManager.getCollection(URI, "miusuario", "Pass!123456");
@@ -66,6 +74,7 @@ public class Ej1 {
 						break;
 					case 4:
 						System.out.println("Opcion 4 seleccionada");
+						ej1.getAutorEditor(servicio);
 						break;
 					case 5:
 						salir = true;
@@ -96,9 +105,13 @@ public class Ej1 {
 		}
 	}
 
+	/**
+	 * Realiza la primera consulta del ejercicio:
+	 * @param servicio
+	 * @throws XMLDBException
+	 */
 	private void getApellidos(XPathQueryService servicio) throws XMLDBException {
 		String query = "for $apellido in distinct-values(doc(\"/db/pruebas/libros.xml\")//libro/autor) return $apellido";
-        //String query = "for $l in doc(\"/db/pruebas/libros.xml\")//libro where $l/precio < 50 return $l/titulo/text()";
 
 		ResourceSet resultado = servicio.query(query);
 		ResourceIterator itera = resultado.getIterator();
@@ -113,9 +126,14 @@ public class Ej1 {
 		}
 	}
 	
+	/**
+	 * Realiza la segunda consulta del ejercicio:
+	 * @param servicio
+	 * @throws XMLDBException
+	 */
 	private void getLibrosEditor(XPathQueryService servicio) throws XMLDBException {
 		String query = "for $l in doc(\"/db/pruebas/libros.xml\")//libro where exists($l/editor) return $l";
-        //String query = "for $l in doc(\"/db/pruebas/libros.xml\")//libro where $l/precio < 50 return $l/titulo/text()";
+		String salida = "";
 
 		ResourceSet resultado = servicio.query(query);
 		ResourceIterator itera = resultado.getIterator();
@@ -125,11 +143,20 @@ public class Ej1 {
 		} else {
 			while (itera.hasMoreResources()) {
 				Resource elemento = itera.nextResource();
-				System.out.println(elemento.getContent());
+				salida += (String)elemento.getContent();
 			}
+			salida = salida.replaceAll("\n", " ");
+			salida = salida.replaceAll("<libro.*?>", "\n\tLibro: ");
+			salida = salida.replaceAll("<.*?>", "");
+			System.out.println(salida);
 		}
 	}
 	
+	/**
+	 * Realiza la tercera consulta del ejercicio:
+	 * @param servicio
+	 * @throws XMLDBException
+	 */
 	private void getLibrosPrecio(XPathQueryService servicio) throws XMLDBException {
 		String query = "for $l in doc(\"/db/pruebas/libros.xml\")//libro where $l/precio < 50 return $l/titulo/text()";
 
@@ -141,8 +168,36 @@ public class Ej1 {
 		} else {
 			while (itera.hasMoreResources()) {
 				Resource elemento = itera.nextResource();
-				System.out.println(elemento.getContent());
+				System.out.println("\t"+elemento.getContent());
 			}
 		}
+	}
+	
+	/**
+	 * Realiza la cuarta consulta del ejercicio:
+	 * @param servicio
+	 * @throws XMLDBException
+	 */
+	private void getAutorEditor(XPathQueryService servicio) throws XMLDBException {
+		String query = "for $l in doc(\"/db/pruebas/libros.xml\")//libro return if(exists($l/autor)) then <libro>{$l/titulo}{$l/autor}</libro> else if(exists($l/editor)) then <libro>{$l/titulo}{$l/editor/afiliacion}</libro> else <libro></libro>";
+		String salida = "";
+		
+		ResourceSet resultado = servicio.query(query);
+		ResourceIterator itera = resultado.getIterator();
+      
+		if (!itera.hasMoreResources()) {
+			System.out.println("No hay resultados");
+		} else {
+			while (itera.hasMoreResources()) {
+				Resource elemento = itera.nextResource();
+				salida += (String)elemento.getContent();
+				
+			}
+		}
+		salida = salida.replaceAll("\n", " ");
+		salida = salida.replaceAll("<libro?>", "\n\tLibro: ");
+		salida = salida.replaceAll("<.*?>", "");
+		System.out.println(salida);
+		
 	}
 }
